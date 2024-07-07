35be983679294ccde03b9f2b7b63399ca3e4b290
WAP Industries <141229412+WAP-Industries@users.noreply.github.com>
2024-07-08 00:25:53 +0800

    Add files via upload

---
+++ src/include.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+#include <regex>
+#include <vector>
+#include <utility>
+#include <unordered_map>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <numeric>
+#include <algorithm>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
+
+#include "transpiler.hpp"
+#include "utils.hpp"
+#include "syntax.hpp"
+#include "transpile.hpp"
