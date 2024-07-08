3b6d38ec0d132185fdc9e0fc13dd11fcd3b76e23
WAP Industries <141229412+WAP-Industries@users.noreply.github.com>
2024-07-08 23:08:22 +0800

    Add files via upload

--- build.bat
+++ build.bat
@@ -4,31 +4,39 @@ setlocal enabledelayedexpansion
 goto :main
 
 :print
-powershell -command "Write-host %1 -ForegroundColor %2"
-exit /b
-
+    powershell -command "Write-host %1 -ForegroundColor %2"
+    exit /b
 
 :main
+    set "params=%*"
+    cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/c cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 
-set "params=%*"
-cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/c cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+    for /f "tokens=3 delims=\" %%a in ("!cd!") do set "username=%%a"
+    set "NEWUSER=C:\Users\%username%"
+    set "dir=%NEWUSER%\CrowJS"
 
-for /f "tokens=3 delims=\" %%a in ("!cd!") do set "username=%%a"
-set "NEWUSER=C:\Users\%username%"
+    call :print "Copying objects to %dir%" Yellow
+    rmdir /s /q "%dir%" > nul 2> nul
+    xcopy %cd% "%dir%" /E /I /Y > nul
+    if %errorlevel% neq 0 (
+        call :print "Failed to copy objects" Red
+        goto :end
+    ) else (
+        call :print "Objects copied successfully" Green
+    )
+    echo. 
 
-set "dir=%NEWUSER%\CrowJS"
-rmdir /s /q "%dir%" > nul 2> nul
-xcopy %cd% "%dir%" /E /I /Y > nul
-cd /d "%dir%"
+    cd /d "%dir%"
 
-call :print "Adding path to system environment variables" Yellow
-echo %PATH% | find /i "%cd%" > nul
-if errorlevel 1 (
-    setx /m PATH "%PATH%;%cd%"
-    call :print "%cd% added to system environment variables" Green
-) else (
-    call :print "%cd% already exists in system environment variables" Yellow
-)
-echo.
+    call :print "Adding path to system environment variables" Yellow
+    echo %PATH% | find /i "%cd%" > nul
+    if errorlevel 1 (
+        setx /m PATH "%PATH%;%cd%"
+        call :print "%cd% added to system environment variables" Green
+    ) else (
+        call :print "%cd% already exists in system environment variables" Yellow
+    )
+    echo.
 
-pause
+:end
+    pause
